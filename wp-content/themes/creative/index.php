6021b75aeb390f56d4c2f183e5c02591154d32f4
Uice Lu <uicestone@gmail.com>
2015-08-24 23:44:14 +0800

    数据动态化

--- wp-content/themes/creative/index.php
+++ wp-content/themes/creative/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>上海君桐股权投资管理有限公司</title>
+    <title><?php bloginfo('sitename'); ?></title>
 
     <!-- Bootstrap Core CSS -->
     <link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/css/bootstrap.min.css" type="text/css">
@@ -17,7 +17,7 @@
     <!-- Custom Fonts -->
     <link href='http://fonts.useso.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='http://fonts.useso.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/font-awesome/css/font-awesome.min.css" type="text/css">
 
     <!-- Plugin CSS -->
     <link rel="stylesheet" href="<?=get_stylesheet_directory_uri()?>/css/animate.min.css" type="text/css">
@@ -74,9 +74,9 @@
     <header>
         <div class="header-content">
             <div class="header-content-inner">
-                <h1>上海君桐股权投资管理有限公司</h1>
+                <h1><?php bloginfo('sitename'); ?></h1>
                 <hr>
-                <p>国内资深投资专家和知名投资机构联合创立的、为中国领先的从事私募股权投资和风险投资业务的私募股权投资机构，是最具市场敏锐度的以拟上市企业私募股权为主要投资对象的投资机构之一。</p>
+                <p><?php bloginfo('description'); ?></p>
                 <a href="#about" class="btn btn-primary btn-xl page-scroll">了解更多</a>
             </div>
         </div>
@@ -86,10 +86,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">值得你的期待</h2>
+                    <?php $about = get_posts('post_type=page&name=about')[0]; ?>
+                    <h2 class="section-heading"><?=$about->post_title?></h2>
                     <hr class="light">
-                    <p class="text-faded">基于长期根植于中国及海外企业私募股权投资的丰富经验，对中国及海外资本市场的充分了解，以及多年运作私募股权投资基金的良好经历和业绩，我们以自有资本和所管理的私募股权和风险投资基金对中国成长型企业进行股权投资，发现、挖掘及提升企业价值、帮助实现企业的价值最大化；同时也使得所管理的基金投资的机会成本最低化、风险最小化、渠道系统化及服务专业化。</p>
-                    <p class="text-faded">君桐凭借专业的投资团队、雄厚的财团LP支持、丰富的资本运作经验以及产业战略投资者联盟，为众多拟在境内上市融资的优秀企业，提供扩张和增长所需的资金,并提供专业的投资管理服务；同时，君桐自身也逐步独创性地摸索出一套以私募股权投资为核心、以特定领域为主导的投资基金管理及企业资本运作和价值发现和挖掘为运营模式的投资项目管理机制。</p>
+                    <?=$about->post_content?>
                 </div>
             </div>
         </div>
@@ -99,34 +99,23 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">为您服务</h2>
+                    <h2 class="section-heading"><?=get_category_by_slug('services')->cat_name?></h2>
                     <hr class="primary">
                 </div>
             </div>
         </div>
         <div class="container">
+            <?php $icons = array('diamond', 'paper-plane', 'newspaper-o'); ?>
+            <?php foreach(get_posts('category_name=services') as $index => $service){ ?>
             <div class="row">
                 <div class="col-lg-4 col-md-6 text-center">
                     <div class="service-box">
-                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
-                        <h3>股权并购基金</h3>
+                        <i class="fa fa-4x fa-<?=$icons[$index]?> wow bounceIn text-primary"></i>
+                        <h3><?=$service->post_title?></h3>
                         <p class="text-muted"></p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
-                        <h3>新三板基金</h3>
-                        <p class="text-muted"></p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 text-center">
-                    <div class="service-box">
-                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
-                        <h3>证券投资基金</h3>
-                        <p class="text-muted"></p>
-                    </div>
-                </div>
+                <?php } ?>
             </div>
         </div>
     </section>
@@ -134,96 +123,23 @@
     <section class="no-padding" id="portfolio">
         <div class="container-fluid">
             <div class="row no-gutter">
+                <?php foreach(get_posts('category_name=projects&posts_per_page=-1') as $project){ ?>
                 <div class="col-lg-4 col-sm-6">
                     <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/1.jpg" class="img-responsive" alt="">
+                        <?=get_the_post_thumbnail($project->ID, null, array('class'=>'img-responsive'))?>
                         <div class="portfolio-box-caption">
                             <div class="portfolio-box-caption-content">
                                 <div class="project-category text-faded">
-                                    Category
+                                    
                                 </div>
                                 <div class="project-name">
-                                    Project Name
+                                    <?=$project->post_title?>
                                 </div>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/2.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/3.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/4.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/5.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <a href="#" class="portfolio-box">
-                        <img src="img/portfolio/6.jpg" class="img-responsive" alt="">
-                        <div class="portfolio-box-caption">
-                            <div class="portfolio-box-caption-content">
-                                <div class="project-category text-faded">
-                                    Category
-                                </div>
-                                <div class="project-name">
-                                    Project Name
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                <?php } ?>
             </div>
         </div>
     </section>
@@ -240,18 +156,19 @@
  -->    <section id="contact">
         <div class="container">
             <div class="row">
+                <?php $contact = get_posts('post_type=page&name=contact')[0]; $segments = explode("\n", $contact->post_content); ?>
                 <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">马上联络</h2>
+                    <h2 class="section-heading"><?=$contact->post_title?></h2>
                     <hr class="primary">
-                    <p>耳听为虚，眼见为实。</p>
+                    <p><?=$segments[0]?></p>
                 </div>
                 <div class="col-lg-4 col-lg-offset-2 text-center">
                     <i class="fa fa-phone fa-3x wow bounceIn"></i>
-                    <p>021-60881113</p>
+                    <p><?=$segments[1]?></p>
                 </div>
                 <div class="col-lg-4 text-center">
                     <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-                    <p><a href="mailto:your-email@your-domain.com">will@jt-capital.cn</a></p>
+                    <p><a href="mailto:<?=$segments[2]?>"><?=$segments[2]?></a></p>
                 </div>
             </div>
         </div>
