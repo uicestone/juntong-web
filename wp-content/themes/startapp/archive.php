7327eeb60445d4c23b86794b9374cb09f67ec747
Uice Lu <uicestone@gmail.com>
2018-05-23 02:41:00 +0800

    列表页初步动态化

--- wp-content/themes/startapp/archive.php
+++ wp-content/themes/startapp/archive.php
@@ -5,22 +5,22 @@
 			<div class="inner">
 				<div class="column">
 					<h1>
-						News
+						新闻
 					</h1>
 				</div>
 
 				<div class="column">
 					<div class="breadcrumbs">
 			          <span>
-			          	<a href="#">
-			          		<span property="name">Home</span>
+			          	<a href="<?=site_url()?>">
+			          		<span property="name">首页</span>
 			          	</a>
 			          </span>
 
 						<i class="material-icons keyboard_arrow_right"></i>
 
 						<span property="itemListElement" typeof="ListItem">
-		          		<span property="name">News</span>
+		          		<span property="name">新闻</span>
 		          	</span>
 					</div>
 				</div>
@@ -31,134 +31,42 @@
 	<div class="container padding-bottom-3x">
 		<div class="row">
 			<div class="col-md-9 col-sm-8">
-
+                <?php while(have_posts()): the_post(); ?>
 				<!-- Tile -->
 				<article class="post-tile">
 					<div class="post-body">
 						<header class="post-header">
-							<div class="column">
+							<!--<div class="column">
 								<a class="post-date" href="#">October 11, 2016</a>by
 								<a href="#">8guild</a>&nbsp;in
 								<a href="#">Investment</a>
-							</div>
+							</div>-->
 
-							<div class="column">
+                            <!--<div class="column">
 								<a class="post-comments" href="#">3&nbsp;<i class="material-icons chat_bubble"></i></a>
-							</div>
+							</div>-->
 						</header>
 
 						<h3 class="post-title">
-							<a href="news-single.html">Are You Ready To Buy A Home</a>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 						</h3>
 
 						<p class="post-excerpt">
-							Nulla finibus sit amet turpis et ullamcorper. Proin sit amet accumsan elit, quis finibus
-							arcu. Morbi pretium velit non
+							<?php the_excerpt(); ?>
 						</p>
 
 						<footer class="post-footer">
 							<div class="tags-links">
-								<a href="#" rel="tag">Business</a>
-								<a href="#" rel="tag">Investment</a>
-								<a href="#" rel="tag">IOT</a>
+                                <?php the_tags(); ?>
 							</div>
 						</footer>
 					</div>
-					<a class="post-thumb" href="news-single.html"><img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-bg.jpg"></a>
+					<a class="post-thumb" href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
 				</article><!-- Tile END -->
+                <?php endwhile; ?>
 
-				<article class="post-tile">
-					<div class="post-body">
-						<header class="post-header">
-							<div class="column">
-								<a class="post-date" href="#">October 11, 2016</a>by <a href="#">8guild</a>&nbsp;in <a
-										href="#" rel="category tag">Podcasts</a>
-							</div>
-
-							<div class="column">
-							</div>
-						</header>
-
-						<h3 class="post-title">
-							<a href="news-single.html">Publikate Net</a>
-						</h3>
-
-						<p class="post-excerpt">
-							Nulla finibus sit amet turpis et ullamcorper. Proin sit amet accumsan elit, quis finibus
-							arcu. Morbi pretium velit non
-						</p>
-
-						<footer class="post-footer">
-							<div class="tags-links">
-								<a href="#" rel="tag">Business</a>
-								<a href="#" rel="tag">Investment</a>
-							</div>
-						</footer>
-					</div>
-					<a class="post-thumb" href="news-single.html"><img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-4.jpg"></a>
-				</article>
-
-				<article class="post-tile">
-					<div class="post-body">
-						<header class="post-header">
-							<div class="column">
-								<a class="post-date" href="#">October 10, 2016</a>by
-								<a href="#">8guild</a>&nbsp;in
-								<a href="#">Technology</a>
-							</div>
-
-							<div class="column"></div>
-						</header>
-
-						<h3 class="post-title">
-							<a href="news-single.html">Computer Monitoring Software A Watchdog Protecting Your
-								Interests</a>
-						</h3>
-
-						<p class="post-excerpt">
-							Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec id ante augue. Sed augue
-							ipsum, varius et ligula eu
-						</p>
-
-						<footer class="post-footer">
-							<div class="tags-links">
-								<a href="#" rel="tag">IOT</a>
-								<a href="#" rel="tag">Tech</a>
-							</div>
-						</footer>
-					</div>
-				</article>
-
-				<article class="post-tile">
-					<div class="post-body">
-						<header class="post-header">
-							<div class="column">
-								<a class="post-date" href="#">October 10, 2016</a>by
-								<a href="#">8guild</a>&nbsp;in
-								<a href="#">Technology</a>
-							</div>
-
-							<div class="column"></div>
-						</header>
-
-						<h3 class="post-title">
-							<a href="news-single.html">How To Put Movies On Iphone</a>
-						</h3>
-
-						<p class="post-excerpt">
-							Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec id ante augue. Sed augue
-							ipsum, varius et ligula eu
-						</p>
-
-						<footer class="post-footer">
-							<div class="tags-links">
-								<a href="#" rel="tag">Investment</a>
-								<a href="#" rel="tag">IOT</a>
-							</div>
-						</footer>
-					</div>
-					<a class="post-thumb" href="news-single.html"><img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-3.jpg"></a>
-				</article>
 			</div>
 
 			<div class="col-md-3 col-sm-4">
@@ -167,93 +75,26 @@
 				<aside class="sidebar" role="complementary">
 					<section class="widget widget_search">
 						<form autocomplete="off" class="search-box">
-							<input name="s" placeholder="Search" type="text" value="">
+							<input name="s" placeholder="搜索" type="text" value="">
 							<button type="submit"><i class="material-icons search"></i></button>
 						</form>
 					</section>
 
 					<section class="widget widget_startapp_recent_posts">
 						<h2 class="widget-title">
-							Recents
+							最近
 						</h2>
-
+                        <?php foreach (get_posts() as $post): ?>
 						<div class="post-item">
-							<a class="post-item-thumb" href="#"><img alt=""
-																	 src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-bg-150x150.jpg"></a>
+							<a class="post-item-thumb" href="<?=get_the_permalink($post->ID)?>"><?=get_the_post_thumbnail($post->ID, 'small')?></a>
 							<div class="post-item-info">
-								<span class="post-item-date">October 11</span>
+                                <!--<span class="post-item-date">October 11</span>-->
 								<h3 class="post-item-title">
-									<a href="#" rel="bookmark">Are You Ready To Buy A Home</a>
+									<a href="<?=get_the_permalink($post->ID)?>" rel="bookmark"><?=get_the_title($post->ID)?></a>
 								</h3>
 							</div>
 						</div>
-
-						<div class="post-item">
-							<a class="post-item-thumb" href="#"><img alt=""
-																	 src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-4-150x150.jpg"></a>
-							<div class="post-item-info">
-								<span class="post-item-date">October 11</span>
-								<h3 class="post-item-title">
-									<a href="#" rel="bookmark">Publikate Net</a>
-								</h3>
-							</div>
-						</div>
-
-						<div class="post-item">
-							<div class="post-item-info">
-								<span class="post-item-date">October 10</span>
-								<h3 class="post-item-title">
-									<a href="#" rel="bookmark">Computer Monitoring Software A Watchdog Protecting Your
-										Interests</a>
-								</h3>
-							</div>
-						</div>
-
-						<div class="post-item">
-							<a class="post-item-thumb" href="#"><img alt=""
-																	 src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-3-150x150.jpg"></a>
-							<div class="post-item-info">
-								<span class="post-item-date">October 10</span>
-								<h3 class="post-item-title">
-									<a href="#" rel="bookmark">How To Put Movies On Iphone</a>
-								</h3>
-							</div>
-						</div>
-					</section>
-
-					<section class="widget widget_recent_comments" id="recent-comments-2">
-						<h2 class="widget-title">
-							Recent Comments
-						</h2>
-
-						<ul id="recentcomments">
-							<li class="recentcomments">
-								<span class="comment-author-link">8guild</span> on <a href="#">Are You Ready To Buy A
-									Home</a>
-							</li>
-
-							<li class="recentcomments">
-								<span class="comment-author-link">8guild</span> on <a href="#">Are You Ready To Buy A
-									Home</a>
-							</li>
-
-							<li class="recentcomments">
-								<span class="comment-author-link">8guild</span> on <a href="#">Are You Ready To Buy A
-									Home</a>
-							</li>
-						</ul>
-					</section>
-
-					<section class="widget widget_archive">
-						<h2 class="widget-title">
-							Archives
-						</h2>
-
-						<ul>
-							<li>
-								<a href='#'>October 2016</a>
-							</li>
-						</ul>
+                        <?php endforeach; ?>
 					</section>
 
 					<section class="widget widget_categories" id="categories-2">
@@ -275,31 +116,7 @@
 							</li>
 						</ul>
 					</section>
-
-					<section class="widget widget_meta">
-						<h2 class="widget-title">
-							Meta
-						</h2>
-
-						<ul>
-							<li>
-								<a href="#">Log in</a>
-							</li>
-
-							<li>
-								<a href="#">Entries <abbr title="Really Simple Syndication">RSS</abbr></a>
-							</li>
-
-							<li>
-								<a href="#">Comments <abbr title="Really Simple Syndication">RSS</abbr></a>
-							</li>
-
-							<li>
-								<a href="#"
-								   title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress.org</a>
-							</li>
-						</ul>
-					</section>
+                    
 				</aside>
 			</div>
 		</div>
