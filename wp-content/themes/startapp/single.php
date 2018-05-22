28dbd8adfc2151c559dba2077529e2a7d1e7ea96
Uice Lu <uicestone@gmail.com>
2018-05-22 13:30:59 +0800

    内容详情页初步动态化

--- wp-content/themes/startapp/single.php
+++ wp-content/themes/startapp/single.php
@@ -16,144 +16,29 @@
 						<!-- Section Start -->
 						<div class="container">
 							<header class="post-header">
-								<div class="column">
+								<!--<div class="column">
 									<a class="post-date" href="#">October 27, 2016</a>by
 									<a href="#">8guild</a>&nbsp;in
 									<a href="#">Banks</a>,
 									<a href="#">Investment</a>
-								</div>
+								</div>-->
 
 								<div class="column"></div>
 							</header>
 
 							<h1 class="post-title">
-								News Single
+								<?php the_title(); ?>
 							</h1>
 
 							<div class="featured-image padding-bottom-1x">
-								<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-bg.jpg">
+                                <?php the_post_thumbnail('post-thumbnail', array('alt' => '')); ?>
 							</div>
 						</div><!-- Section END -->
 
 						<!-- Section Start -->
 						<section class="fw-section">
 							<div class="container">
-								<div class="text-block">
-									<h4>
-										3&nbsp;Maecenas Lobortis, Lorem At Commodo Tincidunt, Elit Arcu Dictum Erat, Nec
-										Pulvinar Mi Augue a Purus
-									</h4>
-
-									<p>
-										Sed finibus fringilla ante sed semper. Sed sit amet ultricies eros, in interdum
-										eros. Suspendisse vitae porta dolor, eget auctor nulla. Phasellus varius velit
-										sed ex ultrices, id consequat sem posuere. Quisque non nulla vitae lectus dictum
-										rutrum eu id mi. Donec urna neque, viverra eget sapien in, maximus lobortis
-										ipsum. Cras pretium nec nisl nec dapibus. Fusce et leo sed dui auctor dignissim
-										sit amet at magna. Nam in tincidunt nibh, at hendrerit lorem. Praesent pulvinar
-										risus ac velit imperdiet tincidunt. Maecenas lobortis, lorem at commodo
-										tincidunt, elit arcu dictum erat, nec pulvinar mi augue a purus. Integer sapien
-										justo, laoreet eu posuere quis, sodales nec purus. Proin sollicitudin, eros
-										venenatis ullamcorper auctor, ipsum metus dignissim metus, ac commodo ante risus
-										at orci. Proin ac libero nec mi dictum tempor. Aliquam vestibulum aliquam risus
-										vel finibus. Maecenas aliquet efficitur lorem a bibendum.
-									</p>
-
-									<p>
-										Sed finibus fringilla ante sed semper. Sed sit amet ultricies eros, in interdum
-										eros. Suspendisse vitae porta dolor, eget auctor nulla. Phasellus varius velit
-										sed ex ultrices, id consequat sem posuere. Quisque non nulla vitae lectus dictum
-										rutrum eu id mi. Donec urna neque, viverra eget sapien in, maximus lobortis
-										ipsum. Cras pretium nec nisl nec dapibus. Fusce et leo sed dui auctor dignissim
-										sit amet at.
-									</p>
-								</div>
-							</div>
-						</section><!-- Section END -->
-
-						<!-- Section Start -->
-						<section class="fw-section">
-							<div class="container">
-								<figure class="single-image wp-caption">
-									<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-1.jpg">
-
-									<figcaption class="wp-caption-text">
-										Developed by the Intel Corporation, HDCP stands for high-bandwidth digital
-										content protection
-									</figcaption>
-								</figure>
-
-								<div class="text-block">
-									<p>
-										Sed finibus fringilla ante sed semper. Sed sit amet ultricies eros, in interdum
-										eros. Suspendisse vitae porta dolor, eget auctor nulla. Phasellus varius velit
-										sed ex ultrices, id consequat sem posuere. Quisque non nulla vitae lectus dictum
-										rutrum eu id mi. Donec urna neque, viverra eget sapien in, maximus lobortis
-										ipsum. Cras pretium nec nisl nec dapibus. Fusce et leo sed dui auctor dignissim
-										sit amet at magna.
-									</p>
-								</div>
-							</div>
-						</section><!-- Section END -->
-
-						<!-- Section Start -->
-						<section class="fw-section">
-							<div class="container">
-								<div class="text-block">
-									<h4>
-										Sed Finibus Fringilla Ante Sed Semper
-									</h4>
-
-									<p>
-										Sed finibus fringilla ante sed semper. Sed sit amet ultricies eros, in interdum
-										eros. Suspendisse vitae porta dolor, eget auctor nulla. Phasellus varius velit
-										sed ex ultrices, id consequat sem posuere. Quisque non nulla vitae lectus dictum
-										rutrum eu id mi. Donec urna neque, viverra eget sapien in, maximus lobortis
-										ipsum. Cras pretium nec nisl nec dapibus. Fusce et leo sed dui auctor dignissim
-										sit amet at magna. Nam in tincidunt nibh, at hendrerit lorem. Praesent pulvinar
-										risus ac velit imperdiet tincidunt. Maecenas lobortis, lorem at commodo
-										tincidunt, elit arcu dictum erat, nec pulvinar mi augue a purus. Integer sapien
-										justo, laoreet eu posuere quis, sodales nec purus:
-									</p>
-								</div>
-
-								<ul class="list-unordered text-lg text-dark">
-									<li>“raesent pulvinar risus ac velit imperdiet tincidunt?”</li>
-									<li>“roin sollicitudin, eros venenatis ullamcorper auctor?”</li>
-									<li>“lectus dictum rutrum eu id?”</li>
-									<li>“onec urna neque, viverra eget sapien in"</li>
-								</ul>
-
-								<figure class="single-image wp-caption">
-									<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-2.jpg">
-									<figcaption class="wp-caption-text">
-										Maintain Your Pc S Performance With Pc Programs
-									</figcaption>
-								</figure>
-
-								<div class="text-block">
-									<h4>
-										Sed Finibus Fringilla Ante
-									</h4>
-
-									<p>
-										Sed finibus fringilla ante sed semper. Sed sit amet ultricies eros, in interdum
-										eros. Suspendisse vitae porta dolor, eget auctor nulla. Phasellus varius velit
-										sed ex ultrices, id consequat sem posuere. Quisque non nulla vitae lectus dictum
-										rutrum eu id mi. Donec urna neque, viverra eget sapien in, maximus lobortis
-										ipsum. Cras pretium nec nisl nec dapibus. Fusce et leo sed dui auctor dignissim
-										sit amet at magna. Nam in tincidunt nibh, at hendrerit lorem.
-									</p>
-
-									<blockquote>
-										<p>
-											Fusce et leo sed dui auctor dignissim sit amet at magna. Nam in tincidunt
-											nibh, at hendrerit lorem. Praesent pulvinar risus ac velit imperdiet
-											tincidunt. Maecenas lobortis, lorem at commodo tincidunt, elit arcu dictum
-											erat, nec pulvinar mi augue a purus.
-										</p>
-									</blockquote>
-								</div>
+								<?php the_content(); ?>
 							</div>
 						</section><!-- Section END -->
 
@@ -161,36 +46,23 @@
 						<div class="container">
 							<footer class="post-footer">
 								<div class="tags-links">
-									<a href="#" rel="tag">Business</a>
+									<!--<a href="#" rel="tag">Business</a>
 									<a href="#" rel="tag">Investment</a>
-									<a href="#" rel="tag">Remote</a>
+									<a href="#" rel="tag">Remote</a>-->
 								</div>
 							</footer>
 
 							<div class="post-share-buttons">
 								<div class="column">
-									<a class="startapp-share-twitter" href="#"><i class="socicon-twitter"></i> Share on
-										Twitter</a>
+									<a class="startapp-share-wechat" href="#"><i class="fa fa-wechat"></i> 分享到微信</a>
 								</div>
-
-								<div class="column">
-									<a class="startapp-share-facebook" href="#"><i class="socicon-facebook"></i> Share
-										on Facebook</a>
-								</div>
-
-								<div class="column">
-									<a class="startapp-share-googleplus" href="#"><i class="socicon-googleplus"></i>
-										Share on Google+</a>
-								</div>
-
-								<div class="column">
-									<a class="startapp-share-pinterest" href="#"><i class="socicon-pinterest"></i> Share
-										on Pinterest</a>
-								</div>
-							</div>
+                                <div class="column">
+                                    <a class="startapp-share-weibo" href="#"><i class="fa fa-weibo"></i> 分享到微博</a>
+                                </div>
+                            </div>
 
 							<!-- Widget Start -->
-							<section class="post-author">
+							<!--<section class="post-author">
 								<div class="post-author-thumb">
 									<a href="#">
 										<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-author.jpg">
@@ -215,67 +87,35 @@
 										<a class="social-btn" href="#"><i class="socicon-facebook"></i></a>
 									</div>
 								</div>
-							</section><!-- Widget END -->
+							</section>--><!-- Widget END -->
 
 							<!-- Widget Start -->
 							<section class="related-posts">
 								<h4 class="text-gray margin-bottom-2x">
-									Related Posts
+									相关新闻
 								</h4>
 
 								<div class="related-post-entry">
 									<header class="post-header">
-										<div class="column">
+										<!--<div class="column">
 											<a class="post-date" href="#">October 29, 2016</a>by
 											<a href="#">8guild</a>&nbsp;in
 											<a href="#" rel="category tag">Investment</a>
-										</div>
+										</div>-->
 
 										<div class="column"></div>
 									</header>
 
 									<h3 class="post-title">
-										<a href="#">3 Easy Ways to Be Creative</a>
+										<a href="">相关新闻标题</a>
 									</h3>
 								</div>
 
-								<div class="related-post-entry">
-									<header class="post-header">
-										<div class="column">
-											<a class="post-date" href="#">October 29, 2016</a>by
-											<a href="#">8guild</a>&nbsp;in
-											<a href="#">Investment</a>
-										</div>
-
-										<div class="column"></div>
-									</header>
-
-									<h3 class="post-title">
-										<a href="#">How Online Banks Work</a>
-									</h3>
-								</div>
-
-								<div class="related-post-entry">
-									<header class="post-header">
-										<div class="column">
-											<a class="post-date" href="#">October 29, 2016</a>by
-											<a href="#">8guild</a>&nbsp;in
-											<a href="#">Business</a>,
-											<a href="#">Investment</a>
-										</div>
-
-										<div class="column"></div>
-									</header>
-
-									<h3 class="post-title">
-										<a href="#">Choose the Right Way</a>
-									</h3>
-								</div>
 							</section><!-- Widget END -->
 						</div><!-- Post Footer END -->
 
 						<!-- Comments Area Start -->
-						<div class="comments-area">
+						<!--<div class="comments-area">
 							<h4 class="text-gray margin-bottom-2x">
 								Comments <sup>4</sup>
 							</h4>
@@ -412,7 +252,7 @@
 									</div>
 								</form>
 							</div>
-						</div><!-- Comme	nts Area Start END -->
+						</div>--><!-- Comme	nts Area Start END -->
 					</article><!-- Post Section END -->
 				</div><!-- Posts Column END -->
 
@@ -427,138 +267,104 @@
 					</section><!-- Widget END -->
 
 					<!-- Widget Start -->
-					<section class="widget widget_startapp_author"
-					">
-					<h2 class="widget-title">
-						Blog Author
-					</h2>
+					<!--<section class="widget widget_startapp_author">
+                        <h2 class="widget-title">
+                            Blog Author
+                        </h2>
 
-					<div class="startapp-author text-left">
-						<img alt="demo-avatar" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-author.jpg">
+                        <div class="startapp-author text-left">
+                            <img alt="demo-avatar" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-author.jpg">
 
-						<p>
-							SaaS &amp; Enterprise, Finance and Payments, “Intelligent Software”, ConsumerPalo Alto
-						</p>
+                            <p>
+                                SaaS &amp; Enterprise, Finance and Payments, “Intelligent Software”, ConsumerPalo Alto
+                            </p>
 
-						<div class="social-bar sb-border sb-rounded sb-dark-skin inline">
-							<a class="social-btn" href="#"><i class="socicon-instagram"></i></a>
-							<a class="social-btn" href="#"><i class="socicon-linkedin"></i></a>
-							<a class="social-btn" href="#"><i class="socicon-odnoklassniki"></i></a>
-							<a class="social-btn" href="#"><i class="socicon-facebook"></i></a>
-						</div>
-					</div>
-	</section><!-- Widget END -->
+                            <div class="social-bar sb-border sb-rounded sb-dark-skin inline">
+                                <a class="social-btn" href="#"><i class="socicon-instagram"></i></a>
+                                <a class="social-btn" href="#"><i class="socicon-linkedin"></i></a>
+                                <a class="social-btn" href="#"><i class="socicon-odnoklassniki"></i></a>
+                                <a class="social-btn" href="#"><i class="socicon-facebook"></i></a>
+                            </div>
+                        </div>
+                    </section>--><!-- Widget END -->
 
-	<!-- Widget Start -->
-	<section class="widget widget_categories"">
-	<h2 class="widget-title">
-		Categories
-	</h2>
+                    <!-- Widget Start -->
+                    <section class="widget widget_categories">
+                        <h2 class="widget-title">
+                            新闻分类
+                        </h2>
+                        <?php wp_list_categories(); ?>
+                    </section><!-- Widget END -->
 
-	<ul>
-		<li class="cat-item">
-			<a href="#">Banks</a> (2)
-		</li>
+                    <!-- Widget Start -->
+                    <section class="widget widget_startapp_recent_posts">
+                        <h2 class="widget-title">
+                            Recent Posts
+                        </h2>
 
-		<li class="cat-item">
-			<a href="#">Business</a> (1)
-		</li>
+                        <div class="post-item">
+                            <a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
+                                <img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-bg-150x150.jpg">
+                            </a>
 
-		<li class="cat-item">
-			<a href="#">CEO Talks</a> (2)
-		</li>
+                            <div class="post-item-info">
+                                <span class="post-item-date">October 29</span>
+                                <h3 class="post-item-title">
+                                    <a href="blog-single-post-right-sidebar.html" rel="bookmark">Incredible City View</a>
+                                </h3>
+                            </div>
+                        </div>
 
-		<li class="cat-item">
-			<a href="#">Investment</a> (13)
-		</li>
+                        <div class="post-item">
+                            <a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
+                                <img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-4-150x150.jpg">
+                            </a>
 
-		<li class="cat-item">
-			<a href="#">IT Thoughts</a> (5)
-		</li>
+                            <div class="post-item-info">
+                                <span class="post-item-date">October 29</span>
+                                <h3 class="post-item-title">
+                                    <a href="blog-single-post-right-sidebar.html" rel="bookmark">Thoughts About Car Concepts</a>
+                                </h3>
+                            </div>
+                        </div>
 
-		<li class="cat-item">
-			<a href="#">Office</a> (4)
-		</li>
+                        <div class="post-item">
+                            <a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
+                                <img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-3-150x150.jpg">
+                            </a>
 
-		<li class="cat-item">
-			<a href="#">Podcasts</a> (1)
-		</li>
+                            <div class="post-item-info">
+                                <span class="post-item-date">October 29</span>
+                                <h3 class="post-item-title">
+                                    <a href="blog-single-post-right-sidebar.html" rel="bookmark">Drones and Delivery</a>
+                                </h3>
+                            </div>
+                        </div>
+	                </section><!-- Widget END -->
 
-		<li class="cat-item">
-			<a href="#">Technology</a> (2)
-		</li>
-	</ul>
-	</section><!-- Widget END -->
+                    <!-- Widget Start -->
+                    <section class="widget widget_recent_comments">
+                        <h2 class="widget-title">
+                            Recent Comments
+                        </h2>
 
-	<!-- Widget Start -->
-	<section class="widget widget_startapp_recent_posts"">
-	<h2 class="widget-title">
-		Recent Posts
-	</h2>
+                        <ul>
+                            <li class="recentcomments">
+                                <span class="comment-author-link">8guild</span> on <a href="#">Withings Camera</a>
+                            </li>
 
-	<div class="post-item">
-		<a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
-			<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-bg-150x150.jpg"">
-		</a>
+                            <li class="recentcomments">
+                                <span class="comment-author-link">8guild</span> on <a href="#">Drones and Delivery</a>
+                            </li>
 
-		<div class="post-item-info">
-			<span class="post-item-date">October 29</span>
-			<h3 class="post-item-title">
-				<a href="blog-single-post-right-sidebar.html" rel="bookmark">Incredible City View</a>
-			</h3>
-		</div>
-	</div>
-
-	<div class="post-item">
-		<a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
-			<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-4-150x150.jpg">
-		</a>
-
-		<div class="post-item-info">
-			<span class="post-item-date">October 29</span>
-			<h3 class="post-item-title">
-				<a href="blog-single-post-right-sidebar.html" rel="bookmark">Thoughts About Car Concepts</a>
-			</h3>
-		</div>
-	</div>
-
-	<div class="post-item">
-		<a class="post-item-thumb" href="blog-single-post-right-sidebar.html">
-			<img alt="" src="<?=get_stylesheet_directory_uri()?>/assets/img/fin-blog-post-3-150x150.jpg">
-		</a>
-
-		<div class="post-item-info">
-			<span class="post-item-date">October 29</span>
-			<h3 class="post-item-title">
-				<a href="blog-single-post-right-sidebar.html" rel="bookmark">Drones and Delivery</a>
-			</h3>
-		</div>
-	</div>
-	</section><!-- Widget END -->
-
-	<!-- Widget Start -->
-	<section class="widget widget_recent_comments"">
-	<h2 class="widget-title">
-		Recent Comments
-	</h2>
-
-	<ul>
-		<li class="recentcomments">
-			<span class="comment-author-link">8guild</span> on <a href="#">Withings Camera</a>
-		</li>
-
-		<li class="recentcomments">
-			<span class="comment-author-link">8guild</span> on <a href="#">Drones and Delivery</a>
-		</li>
-
-		<li class="recentcomments">
-			<span class="comment-author-link">8guild</span> on <a href="#">Pretty Watches</a>
-		</li>
-	</ul>
-	</section><!-- Widget END -->
-	</div><!-- Sidebar Column END -->
-	</div>
-	</div>
+                            <li class="recentcomments">
+                                <span class="comment-author-link">8guild</span> on <a href="#">Pretty Watches</a>
+                            </li>
+                        </ul>
+                    </section><!-- Widget END -->
+	            </div><!-- Sidebar Column END -->
+	        </div>
+	    </div>
 	</section>
 
 <?php get_footer(); ?>
